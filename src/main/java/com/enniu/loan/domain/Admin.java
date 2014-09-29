7351b10113c2a4cf5fb8acf00e07f5323c7b45e9
Evans Yang <ytfei01@foxmail.com>
2014-09-29 16:53:15 +0800

    init project

---
+++ src/main/java/com/enniu/loan/domain/Admin.java
@@ -0,0 +1,10 @@
+package com.enniu.loan.domain;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Admin {
+}
